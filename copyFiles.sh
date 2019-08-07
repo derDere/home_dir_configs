71d65653e521668376a34d5535c3b0461142c50d
Phillip Deremer <phillip@deremer.de>
2019-08-07 21:11:03 +0200

    add i3 and polybar config

--- copyFiles.sh
+++ copyFiles.sh
@@ -1,5 +1,6 @@
-cp ~/.home_dir_configs/.bashrc ~/.bashrc
-cp ~/.home_dir_configs/.emacs ~/.emacs
-cp ~/.home_dir_configs/.mp.mpsl ~/.mp.mpsl
-cp ~/.home_dir_configs/.tmux.conf ~/.tmux.conf
-
+cp -f ~/.home_dir_configs/.bashrc ~/.bashrc
+cp -f ~/.home_dir_configs/.emacs ~/.emacs
+cp -f ~/.home_dir_configs/.mp.mpsl ~/.mp.mpsl
+cp -f ~/.home_dir_configs/.tmux.conf ~/.tmux.conf
+cp -fr ~/.home_dir_configs/i3 ~/.config/i3
+cp -fr ~/.home_dir_configs/polybar ~/.config/polybar
