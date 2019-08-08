1a3e9d694f0bd903fa55606b2a021085ec004339
Phillip Deremer <phillip@deremer.de>
2019-08-08 08:14:05 +0200

    fixed copyFiles.sh

--- copyFiles.sh
+++ copyFiles.sh
@@ -2,5 +2,5 @@ cp -vf ~/.home_dir_configs/.bashrc ~/.bashrc
 cp -vf ~/.home_dir_configs/.emacs ~/.emacs
 cp -vf ~/.home_dir_configs/.mp.mpsl ~/.mp.mpsl
 cp -vf ~/.home_dir_configs/.tmux.conf ~/.tmux.conf
-cp -vfr ~/.home_dir_configs/i3 ~/.config/i3
-cp -vfr ~/.home_dir_configs/polybar ~/.config/polybar
+cp -vfr ~/.home_dir_configs/i3 ~/.config/
+cp -vfr ~/.home_dir_configs/polybar ~/.config/
